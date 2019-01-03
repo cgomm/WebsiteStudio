115cda4cb86d5aa2a80c2efa21cb086573d93741
Christian Gommans <c.gommans@outlook.de>
2019-01-03 16:36:24 +0100

    Created index.php, header.php and footer.php and two subfolders. Those files are the framework and will be changed several times in the future.

---
+++ SourceCode/webApp/index.php
@@ -0,0 +1,10 @@
+<!-- starting php session, including all requiered html header information for the browser interface -->
+<?php
+	session_start();
+	include_once '../php/header.php';
+?>
+
+<!-- space for content -->
+
+	</body>
+</html>
