7184beda96e9201799d782702919cad06dd58b3a
Christian Gommans <c.gommans@outlook.de>
2019-01-04 16:43:46 +0100

    Working on construction page.
    I have added a subtext to our construction page and changed some colors.

--- SourceCode/webApp/errorPages/underConstruction.php
+++ SourceCode/webApp/errorPages/underConstruction.php
@@ -1,3 +1,6 @@
 <div class="d-flex justify-content-center alert alert-danger" role="alert">
 	<h1>This page is under construction. Try again later.</h1>
 </div>
+<div class="d-flex justify-content-center p-2">
+	<h3 class="justify-content-center construction-text">We are setting up our first Homepage about our developer part of our group.</p>
+</div>
--- SourceCode/webApp/index.php
+++ SourceCode/webApp/index.php
@@ -1,7 +1,7 @@
 <!-- starting php session, including all requiered html header information for the browser interface -->
 <?php
 	session_start();
-	include_once('../php/header.php');
+	include('./header/header.php');
 ?>
 
 
