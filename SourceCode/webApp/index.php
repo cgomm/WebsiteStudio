89c8753ab84cacb0435beea1e491b742741c6009
Christian Gommans <c.gommans@outlook.de>
2019-01-05 15:27:52 +0100

    Fixed error codes on index.php

--- SourceCode/webApp/index.php
+++ SourceCode/webApp/index.php
@@ -1,12 +1,12 @@
 <!-- starting php session, including all requiered html header information for the browser interface -->
 <?php
 	session_start();
-	include('./pagesHeader/pagesHeader.php');
+	include('./pages/pagesHeader/header.php');
 ?>
 
 
 <!-- space for content -->
-	<?php include('./pagesError/underConstruction.php');?>
-	<?php include('./pagesFooter/pagesFooter.php');?>
+	<?php include('./pages/pagesError/underConstruction.php');?>
+	<?php include('./pages/pagesFooter/footer.php');?>
 	</body>
 </html>
