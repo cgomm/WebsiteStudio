e83c42ea9b4d6b798d716793be816acbc7951786
McKnacker <felixwill@t-online.de>
2019-01-11 22:08:13 +0100

    Added base content sites.
    Added the base content sites for aboutSTG, team, games, contact. Also changed the header and footer file to be able to include those in the base sites.

--- SourceCode/webApp/pages/pagesHeader/header.php
+++ SourceCode/webApp/pages/pagesHeader/header.php
@@ -3,8 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		<title>ShatteredTimeCoding</title>
-   		<link href="./css/bootstrap.css" rel="stylesheet" type="text/css">
-		<link href="./css/main.css" rel="stylesheet" type="text/css">
+   		<link href="../../css/bootstrap.css" rel="stylesheet" type="text/css">
+		<link href="../../css/main.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
 		
