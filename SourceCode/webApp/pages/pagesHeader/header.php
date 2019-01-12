cba15a01a81e31d1d5e65e4e52979fd81ba17886
McKnacker <felixwill@t-online.de>
2019-01-12 14:37:56 +0100

    Added basic bootstrap navbar.
    Added a basic bootstrap navbar according to documentation and implemented our basic sites into it.

--- SourceCode/webApp/pages/pagesHeader/header.php
+++ SourceCode/webApp/pages/pagesHeader/header.php
@@ -7,4 +7,42 @@
 		<link href="../../css/main.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>
+    <!-- start of navbar -->
+    <!-- ToDo: change the design of the navbar later on accordingly -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="../../index.php">ShatteredTimeCoding</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- ToDo: check the href and see if we can shorten them for performance reasons -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../../index.php">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../../pages/pagesContent/aboutSTG.php">About</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="../../pages/pagesContent/team.php">Team</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="../../pages/pagesContent/games.php">Games</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="../../pages/pagesContent/contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Have fun!</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="../../resources/images/surprise.jpg">   </a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
 		
