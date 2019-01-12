77a3583f7d8dcce2ec9c7abe044558d5de525a98
McKnacker <felixwill@t-online.de>
2019-01-12 14:27:33 +0100

    Removed testing navbar.
    Removed the old navbar from the base sites which were used to test.

--- SourceCode/webApp/pages/pagesContent/team.php
+++ SourceCode/webApp/pages/pagesContent/team.php
@@ -7,27 +7,12 @@
  * WorkedOnBy: Felix Will
  ******************************
  * LastChangeUser: Felix Will
- * LastChangeDate: 11.01.2019
+ * LastChangeDate: 12.01.2019
  ******************************
  */
     include('../pagesHeader/header.php');
 ?>
 <!-- start of content -->
-    <button onclick="location.href='../../index.php'" type="button">
-        Home
-    </button>
-    <button onclick="location.href='aboutSTG.php'" type="button">
-        About
-    </button>
-    <button onclick="location.href='games.php'" type="button">
-        Games
-    </button>
-    <button onclick="location.href='team.php'" type="button">
-        Team
-    </button>
-    <button onclick="location.href='contact.php'" type="button">
-        Contact
-    </button>
 <!-- end of content -->
 <?php
     include('../pagesFooter/footer.php');
